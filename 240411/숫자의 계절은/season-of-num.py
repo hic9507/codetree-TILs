fc1928dad36b95f8676aa1d0d77b7662350b28f4
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-11 01:19:23 +0900

    Title: 숫자의 계절은, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/숫자의 계절은/season-of-num.py
+++ 240411/숫자의 계절은/season-of-num.py
@@ -1,10 +1,20 @@
 m = int(input())
 
-if 3 <= m and m <= 5:
-    print('Spring')
-elif 6 <= m and m <= 8:
-    print('Summer')
-elif 9 <= m and m <= 11:
-    print('Fall')
-else:
+# if 3 <= m and m <= 5:
+#     print('Spring')
+# elif 6 <= m and m <= 8:
+#     print('Summer')
+# elif 9 <= m and m <= 11:
+#     print('Fall')
+# else:
+#     print('Winter')
+
+### 간결한 코드
+if m >= 12 or m <= 2:   # and는 말 안됨. 12 이상이면서 2 이하여야 함.
     print('Winter')
+elif m <= 5:
+    print('Spring')
+elif m <= 8:
+    print('Summer')
+else:
+    print('Fall')
