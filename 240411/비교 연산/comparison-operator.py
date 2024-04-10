a9a5ba1ffa202ee4934d0f2a30bc7e472e37797f
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-11 00:25:46 +0900

    Title: 비교 연산, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/비교 연산/comparison-operator.py
+++ 240411/비교 연산/comparison-operator.py
@@ -1,11 +1,20 @@
 a, b = map(int, input().split())
 
-print(1 if a >= b else 0)
-print(1 if a > b else 0)
-print(1 if b >= a else 0)
-print(1 if b > a else 0)
-print(1 if a == b else 0)
-if a != b:
-    print(1)
-else:
-    print(0)
+#### 삼항연산자
+# print(1 if a >= b else 0)
+# print(1 if a > b else 0)
+# print(1 if b >= a else 0)
+# print(1 if b > a else 0)
+# print(1 if a == b else 0)
+# if a != b:
+#     print(1)
+# else:
+#     print(0)
+
+#### 비교연산자
+print(int(a >= b)) # int 없으면 True, False로 출력
+print(int(a > b))
+print(int(b >= a))
+print(int(b > a))
+print(int(a == b))
+print(int(a != b))
