8043e2be8a5563284f8620159175d8091010521b
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-10 18:19:50 +0900

    Title: 합과 평균, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240410/합과 평균/sum-and-avg.py
+++ 240410/합과 평균/sum-and-avg.py
@@ -1,6 +1,14 @@
+### 1번
 # a, b = map(int, input().split())
-
 # print(a+b, (a+b)/2)
 
-lst = list(map(int, input().split()))
-print(f"{sum(lst)} {sum(lst)/len(lst)}")
+### 2번
+# lst = list(map(int, input().split()))
+# print(f"{sum(lst)} {sum(lst)/len(lst)}")
+
+arr = input().split()
+a = int(arr[0])
+b = int(arr[1])
+
+# print(f"{a + b} {(a + b) / 2}")
+print(a+b, (a+b)/2)
