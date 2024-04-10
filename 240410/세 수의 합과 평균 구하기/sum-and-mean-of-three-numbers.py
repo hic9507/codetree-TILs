48e62c713cb9ad3f54f10e5f632028466e6f8523
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-10 18:26:50 +0900

    Title: 세 수의 합과 평균 구하기, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240410/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.py
+++ 240410/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.py
@@ -1,9 +1,19 @@
+### 1번
 # a, b, c = map(int, input().split())
 
 # print(a + b + c)
 # print((a + b + c) // 3) # 231 ms
 
 ### 2번
-lst = list(map(int, input().split()))
-print(sum(lst))
-print(sum(lst) // len(lst))
+# lst = list(map(int, input().split()))
+# print(sum(lst))
+# print(sum(lst) // len(lst)) # 113 ms
+
+### 3번
+arr = input().split()
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
+
+print(a+b+c)
+print(((a+b+c) // 3))
