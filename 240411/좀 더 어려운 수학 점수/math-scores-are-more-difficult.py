3db298cb8a387e4c88f84b81e314b7705557fd48
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-11 01:46:19 +0900

    Title: 좀 더 어려운 수학 점수, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 240411/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -1,12 +1,17 @@
 a_math, a_eng = map(int, input().split())
 b_math, b_eng = map(int, input().split())
 
-if a_math == b_math:
-    if a_eng > b_eng:
-        print('A')
-    else:
-        print('B')
-elif a_math > b_math:
+# if a_math == b_math:
+#     if a_eng > b_eng:
+#         print('A')
+#     else:
+#         print('B')
+# elif a_math > b_math:
+#     print('A')
+# else:
+#     print('B')
+
+if a_math > b_math or (a_math == b_math and a_eng > b_eng):
     print('A')
 else:
     print('B')
