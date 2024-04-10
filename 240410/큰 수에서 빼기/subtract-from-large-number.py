e156f3d27e3b6987ac21b5279e4ebc9901affd09
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-10 20:13:13 +0900

    Title: 큰 수에서 빼기, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240410/큰 수에서 빼기/subtract-from-large-number.py
+++ 240410/큰 수에서 빼기/subtract-from-large-number.py
@@ -4,13 +4,11 @@
 
 # print(max(a, b) - min(a, b))
 
-inp = input()
-arr = inp.split()
+arr = input().split()
 a = int(arr[0])
 b = int(arr[1])
 
-# 출력
 if a > b:
-	print(a - b)
-if a <= b:
-	print(b - a)
+    print(a - b)
+else:
+    print(b - a)
