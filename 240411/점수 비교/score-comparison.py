bd496755c62386098c717d6fac6fbe7ad6ee3768
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-11 00:35:36 +0900

    Title: 점수 비교, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/점수 비교/score-comparison.py
+++ 240411/점수 비교/score-comparison.py
@@ -6,8 +6,8 @@ a_eng = int(arr0[1])
 b_math = int(arr1[0])
 b_eng = int(arr1[1])
 
-if a_math > b_math and a_eng > b_eng:
-    print(1)
-else:
-    print(0)
-# print(1 if a_math > b_math and a_eng > b_eng else 0)
+# if a_math > b_math and a_eng > b_eng:
+#     print(1)
+# else:
+#     print(0)
+print(1 if a_math > b_math and a_eng > b_eng else 0)
