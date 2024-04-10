f6f4421a4e86218c14c2e7e1105a70914e4baed0
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-11 01:28:48 +0900

    Title: 장학금, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/장학금/scholarship.py
+++ 240411/장학금/scholarship.py
@@ -3,7 +3,7 @@ a, b = int(arr[0]), int(arr[1])
 
 if a >= 90 and b >= 95:
     print(100000)
-elif a >= 90 and 90 <= b:
+elif a >= 90 and 90 <= b:   # and b < 95 적을 필요 없음. elif에서 이미 b >= 95는 충족 안됨.
     print(50000)
 else:
     print(0)
