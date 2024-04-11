bc1a5b0f1d5b44be6f5e43fc79a84ba69bdf2df7
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-11 22:58:15 +0900

    Title: 코로나 메뉴얼, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/코로나 메뉴얼/covid-manual.py
+++ 240411/코로나 메뉴얼/covid-manual.py
@@ -6,16 +6,27 @@ a_flu, a_tem = a_arr[0], int(a_arr[1])
 b_flu, b_tem = b_arr[0], int(b_arr[1])
 c_flu, c_tem = c_arr[0], int(c_arr[1])
 
+# if a_flu == 'Y' and a_tem >= 37:
+#     if (b_flu == 'Y' and b_tem >= 37) or (c_flu == 'Y' and c_tem >= 37):
+#         print('E')
+#     else:
+#         print('N')
+
+# elif b_flu == 'Y' and b_tem >= 37:        ## elif말고 else로 바로 가면 됨.
+#     if c_flu == 'Y' and c_tem >= 37:
+#         print('E')
+#     else:
+#         print('N')
+# else:
+#     print('N') 
+
 if a_flu == 'Y' and a_tem >= 37:
     if (b_flu == 'Y' and b_tem >= 37) or (c_flu == 'Y' and c_tem >= 37):
         print('E')
     else:
         print('N')
-
-elif b_flu == 'Y' and b_tem >= 37:
-    if c_flu == 'Y' and c_tem >= 37:
+else:
+    if (b_flu == 'Y' and b_tem >= 37) and (c_flu == 'Y' and c_tem >= 37):
         print('E')
     else:
         print('N')
-else:
-    print('N')
