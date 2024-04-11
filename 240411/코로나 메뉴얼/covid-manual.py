12893f27512205ec4f765ad90024b0cdab5f2b3f
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-11 22:52:03 +0900

    Title: 코로나 메뉴얼, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240411/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,19 @@
+a_arr = input().split()
+b_arr = input().split()
+c_arr = input().split()
+
+a_flu, a_tem = a_arr[0], int(a_arr[1])
+b_flu, b_tem = b_arr[0], int(b_arr[1])
+c_flu, c_tem = c_arr[0], int(c_arr[1])
+
+if a_flu == 'Y' and a_tem >= 37:
+    if (b_flu == 'Y' and b_tem >= 37) or (c_flu == 'Y' and c_tem >= 37):
+        print('E')
+    else:
+        print('N')
+
+elif b_flu == 'Y' and b_tem >= 37:
+    if c_flu == 'Y' and c_tem >= 37:
+        print('Y')
+    else:
+        print('N')
