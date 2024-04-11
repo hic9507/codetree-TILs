56674b7c04bd5c3e38c19899939dd6ff8338567c
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-11 18:11:24 +0900

    Title: 윤년인가, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/윤년인가/is-leap-year.py
+++ 240411/윤년인가/is-leap-year.py
@@ -1,6 +1,19 @@
 y = int(input())
 
-if y % 4 == 0 and y % 100 != 0 or y % 400 == 0:
-    print('true')
+##### 조건 잘 묶기
+# if y % 4 == 0 and y % 100 != 0 or y % 400 == 0:
+#     print('true')
+# else:
+#     print('false')
+
+##### 중첩조건문
+if y % 4 == 0:
+    if y % 100 == 0:
+        if y % 400 == 0:
+            print('true')
+        else:
+            print('false')
+    else:
+        print('true')
 else:
     print('false')
