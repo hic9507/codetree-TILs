7934f08c15b5bf693f5e78ab5befd14a0c03c173
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-12 23:28:58 +0900

    Title: 정수 입력받아 배수 출력, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240412/정수 입력받아 배수 출력/print-multiple-of-input.py
+++ 240412/정수 입력받아 배수 출력/print-multiple-of-input.py
@@ -1,9 +1,4 @@
 n = int(input())
 
-cnt = 0
-for i in range(n, 101, 7):
-    if i % 7 == 0:
-        cnt += 1
-        print(i, end=" ")
-    if cnt == 5:
-        break
+for i in range(n, n*5+1, n):
+    print(i, end=" ")
