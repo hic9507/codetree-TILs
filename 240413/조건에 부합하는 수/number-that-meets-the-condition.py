c739d9b272d91611d87d26cccd87dd9b111b2888
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-13 16:47:49 +0900

    Title: 조건에 부합하는 수, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240413/조건에 부합하는 수/number-that-meets-the-condition.py
+++ 240413/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -1,10 +1,17 @@
 a = int(input())
 
+#### if문 많이
+# for i in range(1, a+1):
+#     if i % 2 == 0 and i % 4 != 0:
+#         continue
+#     if (i // 8) % 2 == 0:
+#         continue
+#     if (i % 7) < 4:
+#         continue
+#     print(i, end=" ")
+
+#### 조건 한 번에 묶기
 for i in range(1, a+1):
-    if i % 2 == 0 and i % 4 != 0:
-        continue
-    if (i // 8) % 2 == 0:
-        continue
-    if (i % 7) < 4:
+    if (i % 2 == 0 and i % 4 != 0) or ((i // 8) % 2 == 0) or ((i % 7) < 4):
         continue
     print(i, end=" ")
