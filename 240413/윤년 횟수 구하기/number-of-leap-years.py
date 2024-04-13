8335cd6951f6f00896bdd5205f1659dcab017d7a
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-13 14:42:35 +0900

    Title: 윤년 횟수 구하기, Time: 103ms, Memory: 25MB, Status: Passed - CodeTree

--- 240413/윤년 횟수 구하기/number-of-leap-years.py
+++ 240413/윤년 횟수 구하기/number-of-leap-years.py
@@ -2,7 +2,15 @@ n = int(input())
 
 cnt = 0
 
+##### 방법 1. 조건 한 번에 작성
 for i in range(1, n+1):
     if i % 4 == 0 and i % 100 != 0 or i % 400 == 0: ### 평년: i % 100 == 0 and i % 400 != 0 
         cnt += 1
 print(cnt)
+
+# for i in range(1, n+1):
+#     if i % 4 == 0:
+#         if i % 100 == 0:        # 104년은 윤년인데 여기서 막혀서 안됨
+#             if i % 400 == 0:
+#                 cnt += 1
+# print(cnt)
