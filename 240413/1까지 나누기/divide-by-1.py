8ca2a8a10b8786aadb3c406f4f39335792e03774
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-13 17:30:37 +0900

    Title: 1까지 나누기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240413/1까지 나누기/divide-by-1.py
+++ 240413/1까지 나누기/divide-by-1.py
@@ -1,9 +1,16 @@
 n = int(input())
 cnt = 0
 
-for i in range(1, n+1):
-    n //= i
+#### for문
+# for i in range(1, n+1):
+#     n //= i
+#     cnt += 1
+#     if n <= 1:
+#         print(cnt)
+#         break
+
+#### while문
+while n >= 1:
     cnt += 1
-    if n <= 1:
-        print(cnt)
-        break
+    n //= cnt
+print(cnt)
