3a77c0b59321c7adbdce35c846a2f4900a167131
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-13 14:15:34 +0900

    Title: 청소당번 정하기, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240413/청소당번 정하기/cleaning-numbering.py
+++ 240413/청소당번 정하기/cleaning-numbering.py
@@ -4,9 +4,11 @@ cnt2, cnt3, cnt12 = 0, 0, 0
 for i in range(n+1):
     if i != 0 and i % 12 == 0:
         cnt12 += 1
+
     else:
         if i != 0 and i % 3 == 0:
             cnt3 += 1
+            
         elif i != 0 and i % 2 == 0:
             cnt2 += 1
 
