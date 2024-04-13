dfbb8432b6f239347cb21dac7cb865639eaf6b27
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-14 01:29:23 +0900

    Title: 별표 출력하기 4, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240414/별표 출력하기 4/print-star-4.py
+++ 240414/별표 출력하기 4/print-star-4.py
@@ -1,12 +1,25 @@
 n = int(input())
-cnt = n
 
-for i in range(n*2-1):
-    for j in range(cnt):
-        print('*', end=" ")
+##### 방법 1.
+# cnt = n
+
+# for i in range(n*2-1):
+#     for j in range(cnt):
+#         print('*', end=" ")
+#     print()
+
+#     if i < (n*2-1)//2:
+#         cnt -= 1
+#     else:
+#         cnt += 1
+
+##### 방법 2.
+for i in range(n):
+    for j in range(n-i):
+        print("*", end=" ")
     print()
 
-    if i < (n*2-1)//2:
-        cnt -= 1
-    else:
-        cnt += 1
+for i in range(n-1, 0, -1):
+    for j in range(n-i+1):
+        print("*", end=" ")
+    print()
