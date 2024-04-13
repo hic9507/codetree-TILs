259d691f4b413360710afd8838af7e1f5e83217a
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-13 13:18:40 +0900

    Title: a 부터 b 까지, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240413/a 부터 b 까지/a-to-b.py
@@ -0,0 +1,10 @@
+arr = input().split()
+a, b = int(arr[0]), int(arr[1])
+
+while a <= b:
+    if a % 2 == 1:
+        print(a, end=" ")
+        a *= 2
+    else:
+        print(a, end=" ")
+        a += 3
