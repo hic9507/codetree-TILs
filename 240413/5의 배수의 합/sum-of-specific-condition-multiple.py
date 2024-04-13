2b1c3f02763c4b3a97117c38138ee360ebb2841c
hic9507 <65755028+hic9507@users.noreply.github.com>
2024-04-13 14:59:05 +0900

    Title: 5의 배수의 합, Time: 155ms, Memory: 24MB, Status: Passed - CodeTree

--- 240413/5의 배수의 합/sum-of-specific-condition-multiple.py
+++ 240413/5의 배수의 합/sum-of-specific-condition-multiple.py
@@ -6,7 +6,7 @@ if a <= b:
         if i % 5 == 0:
             sum_val += i
 else:
-    for i in range(b, a-1, -1):
+    for i in range(a, b-1, -1):
         if i % 5 == 0:
             sum_val += i
 
